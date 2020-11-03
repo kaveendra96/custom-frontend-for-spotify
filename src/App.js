3bc6c021716b8456796a1a71c23348c456d996a0
kaveendra96 <kaveendra96@gmail.com>
2020-11-03 15:08:11 +0530

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+const API_ADDRESS='https://spotify-api-wrapper.appspot.com';
 
 class App extends React.Component {
   state={
-    artistQuery:''
+    artistQuery:'',
+    artist:null,
+    tracks:[]
   }
 
   updateArtistQuery=  event =>{
@@ -12,7 +15,22 @@ class App extends React.Component {
       })
   }
   searchArtist=()=>{
-    console.log(this.state)
+      fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+        .then(response => response.json())
+        .then(json => {
+            if(json.artists.total>0){
+              const artist=json.artists.items[0];
+              this.setState({
+                artist:artist
+              })
+              console.log(artist)
+              fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+                .then(response => response.json())
+                .then(json =>this.setState({tracks:json.tracks}) )
+                .catch(err => alert(err.message))
+            }
+        })
+
   }
   handleKeyPress=event=>{
     if(event.key === 'Enter')
@@ -29,7 +47,7 @@ class App extends React.Component {
               onKeyPress={this.handleKeyPress}
               onChange={this.updateArtistQuery}
               />
-              <button onClick={this.searchArtist} className="btn btn-primary m-2">Search</button>
+              <button onClick={this.searchArtist} className="btn btn-outline-success btn-lg m-2">Search</button>
             </div>
         </div>
       );
